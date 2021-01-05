32ddce721412bed728b04d17534f6845584d3020
miyesven <miyesven@gmail.com>
2021-01-05 00:50:29 -0800

    starting adapting realsense ball tracking code to make a ROS node

--- src/target-tracking-pkg/src/ball_tracking.py
+++ src/target-tracking-pkg/src/ball_tracking.py
@@ -10,11 +10,12 @@ from std_msgs.msg import Int32, Int32MultiArray
 ##### BALL RECOGNITION #####
 # import the necessary packages
 from collections import deque
-from imutils.video import VideoStream
+import pyrealsense2 as rs
 import numpy as np
-import math
-import argparse
-import imutils
+import cv2
+import os
+import sys
+import matplotlib.pyplot as plt
 import time
 
 #import sys
@@ -22,154 +23,143 @@ import time
 import cv2
 #sys.path.append('/opt/ros/kinetic/lib/python2.7/dist-packages')
 
-greenLower = (33, 94, 96)
-greenUpper = (67, 255, 255)
-redLower = (140, 154, 65)
-redUpper = (207, 255, 195)
-center_x = 250 # center of screen, x coord
-center_y = 250 # center of screen, y coord
+##### GLOBAL VARIABLES #####
+redLower = (130, 130, 0)
+redUpper = (255, 255, 255)
+x_list = []
+y_list = []
+radius_list = []
+distance_list = []
+timestamp_list = []
+# args = {}
+# pts = deque()
 
-args = {}
-pts = deque()
+## Takes in an img given by cv2.imread and applies the following filters
+## Returns the filtered image
+def contour_filter(img):
+    blurred = cv2.bilateralFilter(img,12,125,125)
+    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, redLower, redUpper)
+    return mask
 
-def init_webcam():
-	'''Returns videostream'''
-	# construct the argument parse and parse the arguments
-	ap = argparse.ArgumentParser()
-	ap.add_argument("-v", "--video",
-		help="path to the (optional) video file")
-	ap.add_argument("-b", "--buffer", type=int, default=64,
-		help="max buffer size")
-	args = vars(ap.parse_args())
+## Takes in [mask] a binary image (1/0 for each pixel) and [img] the 2D color image
+##  Finds the contours and appends the x,y,radius,timestamp lists from results of each image
+##  MODIFIES [img] by drawing circle and centroid on image
+## Returns [circle] a list for information about enclosing circle (x,y,radius)
+def parse_color_image(mask, img):
+    ## Finding Contours
+    contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # Find max contour area
+    i = 0
+    maxContour = 0
+    maxContourArea = 0
+    for contour in contours:
+        contourArea = cv2.contourArea(contour)
+        if contourArea > maxContourArea:
+            maxContourArea = contourArea
+            maxContour = i
+        i += 1
+    # Find coordinates + radius of min enclosing circle of blob in mask
+    ((x, y), radius) = cv2.minEnclosingCircle(contours[maxContour])
+    # Drawing on image the circle & corresponding centroid calculated above if circle is detected
+    # Also populating x,y,radius lists
+    if radius > 0:
+        img = cv2.circle(img, (int(x), int(y)), int(radius), (0, 255, 255), 2)
+        img = cv2.circle(img, (int(x), int(y)), 5, (0, 0, 255), -1)
+        x_list.append(x)
+        y_list.append(y)
+        radius_list.append(radius)
+        # Define list to return
+        circle = [x,y,radius]
+        return circle
+    else:
+        return None
 
-	# define the lower and upper boundaries of the "green"
-	# ball in the HSV color space, then initialize the
-	# list of tracked points
-	pts = deque(maxlen=args["buffer"])
-	# if a video path was not supplied, grab the reference
-	# to the webcam
-	if not args.get("video", False):
-		vs = VideoStream(src=0).start()
-	# otherwise, grab a reference to the video file
-	else:
-		vs = cv2.VideoCapture(args["video"])
-	# allow the camera or video file to warm up
-	time.sleep(2.0)
-
-	return vs
-
-
-def target_distance_calc(radius):
-	if radius > 1:
-		return 1/radius * 500
-	else:
-		return 500
-
-
-def target_rel_angle_calc(distance, center):
-	angle = []
-	try:
-		angle = [math.degrees(math.atan(np.abs(center_x - center[0])/distance)), math.degrees(math.atan(np.abs(center_y - center[1])/distance))]
-	except:
-		pass
-	return angle
-
-
+## Main ball_tracking node.
+##  Opens stream/video and executes the logic to parse each frame
+## Returns [circle] tuple of (x,y,radius,distance(from depth))
 def ball_tracker():
+	### ROS INTERFACE ###
+	##<------TODO------>##
+	# Subscribe to the following topics:
+	# 	1. Realsense Camera Node Package > Color Frame Topic  
+	# Publish the following topics:
+	# 	1. Tuple
+	# 	2. Time 
 	target_dist_pub = rospy.Publisher('target_distance', Int32, queue_size = 10)
 	target_angle_pub = rospy.Publisher('target_relative_angle', Int32MultiArray, queue_size = 10)
 	rospy.init_node('ball_tracker', anonymous=True)
 	r = rospy.Rate(10)
 
-	vs = init_webcam()
 
 	while not rospy.is_shutdown():
 
 		# grab the current frame
 		frame = vs.read()
-		# handle the frame from VideoCapture or VideoStream
-		frame = frame[1] if args.get("video", False) else frame
-		# if we are viewing a video and we did not grab a frame,
-		# then we have reached the end of the video
-		if frame is None:
-			break
-		# resize the frame, blur it, and convert it to the HSV
-		# color space
-		frame = imutils.resize(frame, width=600)
-		blurred = cv2.GaussianBlur(frame, (11, 11), 0)
-		hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
-		# construct a mask for the color "green", then perform
-		# a series of dilations and erosions to remove any small
-		# blobs left in the mask
-		mask = cv2.inRange(hsv, greenLower, greenUpper)
-		mask = cv2.erode(mask, None, iterations=2)
-		mask = cv2.dilate(mask, None, iterations=2)
+		try:
+        config = rs.config()
+        config.enable_device_from_file(file)
+        pipeline = rs.pipeline()
+        # config.enable_stream(rs.stream.depth, 1280, 720, rs.format.z16, 30)
+        pipeline.start(config)
+        # create colorizer object
+        colorizer = rs.colorizer()
+        time.sleep(2.0)
 
-		# find contours in the mask and initialize the current
-		# (x, y) center of the ball
-		cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-			cv2.CHAIN_APPROX_SIMPLE)
-		cnts = imutils.grab_contours(cnts)
-		center = None 
-		radius = 0
-		# only proceed if at least one contour was found
-		if len(cnts) > 0:
-			# find the largest contour in the mask, then use
-			# it to compute the minimum enclosing circle and
-			# centroid
-			c = max(cnts, key=cv2.contourArea)
-			((x, y), radius) = cv2.minEnclosingCircle(c)
-			M = cv2.moments(c)
-			center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-			# only proceed if the radius meets a minimum size
-			if radius > 10:
-				# draw the circle and centroid on the frame,
-				# then update the list of tracked points
-				cv2.circle(frame, (int(x), int(y)), int(radius),
-					(0, 255, 255), 2)
-				cv2.circle(frame, center, 5, (0, 0, 255), -1)
-		# update the points queue
-		pts.appendleft(center)
+        while True:
+            # get frameset of depth
+            frames = pipeline.wait_for_frames()
+            # get depth frame
+            depth_frame = frames.get_depth_frame()
+            # Colorize depth frame to jet colormap
+            depth_color_frame = colorizer.colorize(depth_frame)
+            # Convert depth_frame to numpy array to render image in opencv
+            depth_color_image = np.asanyarray(depth_color_frame.get_data())
+            # get color frame and convert it to a numpy array
+            color_frame = frames.get_color_frame()
+            color_image = np.asanyarray(color_frame.get_data())
+            color_image = cv2.cvtColor(color_image, cv2.COLOR_BGR2RGB)
 
-		# Publish Results on ROS Node
+            if  color_frame is None or depth_frame is None:
+                break
+
+            # parsing color image
+            mask = contour_filter(color_image)
+            # gets the x,y,radius reading from image
+            circle_list = parse_color_image(mask, color_image)
+
+            if circle_list is None:
+                break
+
+            # get the timestamp of the current color/depth frame
+            timestamp_list.append(color_frame.get_timestamp())
+            # get the depth reading from the depth frame at circle centroid (units: meters) and save into tuple
+            distance = depth_frame.get_distance(int(circle_list[0]), int(circle_list[1]))
+            distance_list.append(distance)
+            circle_list.append(distance)
+            circle = tuple(circle_list)
+            # Render image in opencv window
+            cv2.imshow("Depth Stream", depth_color_image)
+            cv2.imshow("Color Stream", color_image)
+
+            key = cv2.waitKey(1) & 0XFF
+            if key == ord('q'):
+                cv2.destroyAllWindows()
+                break
+
+        ### Publish Results on ROS Node ###
 		target_distance = target_distance_calc(radius)
 		target_relative_angle = target_rel_angle_calc(target_distance, center)
  
 		target_dist_pub.publish(target_distance)
 		target_angle_pub.publish(data=target_relative_angle)
 
-#		# loop over the set of tracked points
-#		for i in range(1, len(pts)):
-#			# if either of the tracked points are None, ignore
-#			# them
-#			if pts[i - 1] is None or pts[i] is None:
-#				continue
-#			# otherwise, compute the thickness of the line and
-#			# draw the connecting lines
-#			thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
-#			cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
-
-#		# record to parse min and max contour of ball
-#		if (startflag == 1):
-#		    minMaxRadius.append(radius);
-
-#		# show the frame to our screen
-#		cv2.imshow("Frame", frame)
-#		key = cv2.waitKey(1) & 0xFF
-#		# if the 'q' key is pressed, stop the loop
-#		if key == ord("q"):
-#			break
-
 		r.sleep()
 
-	# if we are not using a video file, stop the camera video stream
-	if not args.get("video", False): 
-		vs.stop()
-	# otherwise, release the camera
-	else:
-		vs.release()
-	# close all windows
-	cv2.destroyAllWindows()
+    finally:
+        pass
+    
+    return circle
 
 if __name__ == '__main__':
 	try:
